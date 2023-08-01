379e180098bafea430d6d2af9668c6ca1e9d4934
neuralworm <nerualworm@protonmail.com>
2023-08-01 11:15:28 -0700

    Default colors picker added and is now default.  User can switch to custom colors mode.

---
+++ src/lib/components/DefaultColorPallette.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {defaultColors, defaultSteps} from '../data/DefaultColors'
+</script>
+
+<div >
+    {#each defaultColors as color}
+        <div class="flex flex-row">
+            {#each defaultSteps as step}
+                <div class={`bg-${color}-${step}}`}></div>
+            {/each}
+        </div>
+    {/each}
+</div>
---
+++ src/lib/components/ModeToggle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let toggleMode: Function
+</script>
+<div class="p-4 flex flex-row items-center">
+    <button>
+        Default Colors
+    </button>
+    <button>
+        Custom Colors
+    </button>
+
+</div>
