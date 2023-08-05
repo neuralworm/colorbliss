dcf16e7a8b3c8eaef36f6dd9278cac4bef9bc6d2
neuralworm <nerualworm@protonmail.com>
2023-08-05 09:41:38 -0700

    favorites page styling and parsing of favorited gradient example objects.

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -2,16 +2,37 @@
     import { loadFavorites } from "$lib/storage/storage";
     import {favorites} from '$lib/state/store'
     import { onMount } from "svelte";
+    import ExampleView from "$lib/components/examples/ExampleView.svelte";
+    import { examples, type Example } from "$lib/data/examples/Examples";
     let favs: string[]|null
     onMount(()=>{
         loadFavorites()
     })
+    let favoritedExamples: Example[]
     favorites.subscribe((val)=>{
         favs = val
+        favoritedExamples = examples.filter((val: Example) => {
+            if(favs?.includes(val.name)) return true
+            return false
+        })
     })
 </script>
 <section id="favorites-page">
-    {#each favs as fav}
-        {fav}
-    {/each}
+    <section id="favorites-head" class="mx-auto max-w-screen-xl">
+        <h1 class="text-5xl md:text-7xl font-extrabold tracking-tight py-16">
+
+            FAVORITES
+        </h1>
+    </section>
+    {#if favoritedExamples.length == 0}
+        <div>
+            No Favorites Yet
+        </div>
+    {/if}
+    <div class="max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mx-auto mt-24">
+
+        {#each favoritedExamples as fav}
+        <ExampleView example={fav}></ExampleView>
+        {/each}
+    </div>
 </section>
